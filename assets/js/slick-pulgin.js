01d24b7b0d8374c2254e2d20fe9f2c8d49c1a208
TheDuy <dieptheduy2000@gmail.com>
2021-01-09 00:46:32 +0700

    ban2

---
+++ assets/js/slick-pulgin.js
@@ -0,0 +1,18 @@
+$('.home-page-bander-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false ,
+    vertical: true,
+    
+  });
+
+  $('.home-page-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    arrows:false,
+    adaptiveHeight:true,
+
+  });
