fa44cf77e081df3f4840ccb1ff58a070f78b6c89
TheDuy <dieptheduy2000@gmail.com>
2021-01-12 11:47:56 +0700

    b5

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,4 +56,24 @@ $(document).ready(function(){
         $('#wp-home-page-best-seller .best-seller').addClass('visible animate__animated animate__slideInUp');
       }
    });
+        
 });
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 80){
+      $('#wp-policy-menu').addClass('sticky animate__animated animate__fadeIn');  
+      if($(window).scrollTop() > 4300){
+        //$('#wp-policy-menu').toggleClass('sticky animate__animated animate__fadeIn');
+        $('#wp-policy-menu').addClass('sticky animate__animated animate__fadeOut animate__faster');       
+      }else{      
+        $('#wp-policy-menu').removeClass('animate__fadeOut animate__faster');
+      } 
+    }else{
+      $('#wp-policy-menu').removeClass('sticky animate__animated animate__fadeIn');
+    }
+    
+    
+  });
+});
+
