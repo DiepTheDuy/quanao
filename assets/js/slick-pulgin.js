9060aa567504d82285ffe4923bd6575068c62a6e
zuradel <53078525+zuradel@users.noreply.github.com>
2021-01-17 23:58:36 +0700

    gôp collection

--- assets/js/slick-pulgin.js
+++ assets/js/slick-pulgin.js
@@ -1,49 +1,67 @@
-$('.home-page-bander-slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false ,
-    
-    
-  });
+$(".home-page-bander-slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+});
 
-  $('.home-page-slider.new-colection').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows:true,
-    adaptiveHeight:true,
+$(".home-page-slider.new-colection").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+  adaptiveHeight: true,
+});
 
-  });
-  
-  $('.home-page-slider.new-product').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 3,
-    arrows:true,
-    adaptiveHeight:true,
+$(".home-page-slider.new-product").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 3,
+  arrows: true,
+  adaptiveHeight: true,
+});
 
-  });
+//====collection====
+$(".collection-page-slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+});
+$(".gallery-slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  centerPadding: "0px",
+  asNavFor: ".gallery-slider-nav",
+});
+$(".gallery-slider-nav").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: ".gallery-slider-for",
+  dots: true,
+  centerPadding: "0px",
+  centerMode: true,
+  focusOnSelect: true,
+});
 
-  
-
-
- 
-
-  $('.product-view-slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false ,
-    draggable: false ,
-    fade: true,
-    asNavFor: '.product-view-slider-nav'
-  });
-  $('.product-view-slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.product-view-slider-for',    
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
+$(".product-view-slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  draggable: false,
+  fade: true,
+  asNavFor: ".product-view-slider-nav",
+});
+$(".product-view-slider-nav").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".product-view-slider-for",
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+});
